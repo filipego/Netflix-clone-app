526a1d9a4704bbfcda10a521dbcd5e47e56b9754
Filipe Valente Gomes <filipego@gmail.com>
2022-06-12 03:22:24 -0400

    added thumbnails

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
+import Row from '../components/Row'
 
 
 interface Props {
@@ -29,7 +30,7 @@ const Home = ({
 
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]">
+    <div className="relative h-screen bg-gradient-to-b">
       <Head>
         <title>Netflix clone  App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -37,13 +38,15 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className="md:space-y-24">
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
